8bb6cfd034812ec35658676c130fbe220b8ce41f
Martin <martinn.lindbladd@gmail.com>
2021-06-09 16:57:47 +0200

    Added map_sync page & logic. Added temp mapevent_data csv

--- action/views.py
+++ action/views.py
@@ -40,6 +40,7 @@ from django.contrib.auth.models import User
 from .geo_view import geo_view_handler, geo_date_view_handler, geo_update_view, geo_update_post, geo_search, geo_invalid, translate_maplink
 from .start_view import start_view_handler
 from .overview_view import latest_reports_view, locations_view
+from .map_sync import to_fff, eventmap_data
 
 class HomeView(FormView):
   class LocationSearchForm(forms.Form):
