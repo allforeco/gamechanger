dcc834154aa81a0550de1d489a36592b911ebe03
Jan Lindblad <jan.lindblad@for.eco>
2021-07-27 23:04:40 +0200

    Made sure gathering beloging was taken into account in geo_view. This removes a fair amount of doubles.

--- action/geo_view.py
+++ action/geo_view.py
@@ -40,7 +40,9 @@ def geo_view_handler(request, locid):
     raw_witness_list = list(Gathering_Witness.objects.filter(gathering=gathering))
     #print(f"TOWI {gathering} {len(raw_witness_list)} {raw_witness_list}")
     for w in raw_witness_list:
-      witness_dict[(w.gathering.regid,w.date)] = w
+      belong = Gathering_Belong.objects.filter(regid=w.gathering.regid).first()
+      witness_dict[(belong.gathering.regid,w.date)] = w
+      #print(f"TOWD {(belong.gathering.regid,w.date)} = {w}")
   witness_list = list(witness_dict.values())
   witness_list.sort(key=lambda e: e.date, reverse=True)
   total_participants = sum([w.participants for w in witness_list if w.participants])
