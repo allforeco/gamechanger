b73046a0a581a44603ff0d3e08f5dc7865003e62
Gr8m8 <martinn.lindbladd@gmail.com>
2024-07-24 16:00:55 +0200

    event list gathering view

--- action/gathering_view.py
+++ action/gathering_view.py
@@ -10,19 +10,30 @@ ___organization view by id
 '''
 def gathering_view(request, regid):
   template = loader.get_template('action/gathering_overview.html')
-  print('regid', regid)
+  #print('regid', regid)
   gathering = Gathering.objects.get(regid=regid)
-  gathering_witness_list = Gathering_Witness.objects.filter(gathering=gathering).order_by('-date')[:100]
+  event_main_head = Gathering.datalist_template(model=True, date=True, date_end=True, gtype=True, note_address=True, note_time=True, location=True, map_link=True, orgs=True, recorded=True)
+  event_main = [Gathering.datalist(gathering, False, event_main_head)]
+  gathering_witness_list = Gathering_Witness.objects.filter(gathering=gathering).order_by('-date')
+  event_record_list = []
+  event_record_head = Gathering.datalist_template(model=True, date=True, recorded_link=True, location=False, participants=True,recorded=True)
+  for gw in gathering_witness_list:
+    event_record_list.append(Gathering.datalist(gw, True, event_record_head))
+
   organization = None
   organization_view = None
   if gathering.organizations:
     organization = gathering.organizations.first()
 
   context = {
-    'gathering':gathering,
-    'gathering_witness_list':gathering_witness_list,
+    'gathering': gathering,
+    'event_main_head': event_main_head,
+    'event_main': event_main,
+    'event_record_head': event_record_head,
+    'event_record_list': event_record_list,
     'organization':organization,
-    'gathering_types':Gathering.gathering_type.field.choices
+    'gathering_types':Gathering.gathering_type.field.choices,
+    'logginbypass': True,
     }
 
   return HttpResponse(template.render(context, request))
