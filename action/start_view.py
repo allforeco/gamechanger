d8d626df1268caf2ebdcda5aede99b309daeb444
Jan Lindblad <jan.lindblad@for.eco>
2025-02-16 14:15:33 +0100

    Improved start view event selection

--- action/start_view.py
+++ action/start_view.py
@@ -15,10 +15,10 @@ ___lists
 '''
 def start_view_handler(request):
   filter_weeks = datetime.timedelta(weeks=2) #int(request.POST.get('filter_weeks','2'))
-  today = datetime.datetime.today()
+  today = datetime.date.today()
   event_list = []
 
-  gathering_list = Gathering.objects.filter(end_date__range=[today-filter_weeks, today]).order_by("-start_date")
+  gathering_list = Gathering.objects.filter(end_date__gte=today-filter_weeks).order_by("-start_date")
 
   #EVENT WITNESSING LOGIC
   event_record_head = Gathering.datalist_template(
@@ -34,7 +34,7 @@ def start_view_handler(request):
     event_list.append(Gathering.datalist(gw, True, event_record_head, green=True if gw.participants else False))
   for gathering in gathering_list:
     witnesses_here = Gathering_Witness.get_witnesses(gathering, event_record_head, already_listed=gathering_witness_list)
-    event_list += witnesses_here
+    event_list += [e for e in witnesses_here if e['date'] >= today-filter_weeks and e['date'] <= today]
 
   event_list.sort(key=lambda e: e['date'], reverse=True)
 
